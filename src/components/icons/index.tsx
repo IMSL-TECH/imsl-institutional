e2c347ae94bcc861cc6d31e9af303322518ba26d
Marcos Ramos <marcosperes.contato@gmail.com>
2025-04-22 16:27:20 -0300

    feat-created new default icon and created index for icons

---
+++ src/components/icons/index.tsx
@@ -0,0 +1,8 @@
+import  Facebook  from "./facebook";
+import Instagram from "./instagram";
+import YouTube from "./youtube";
+import Default  from "./default";
+
+
+
+export {Facebook,Instagram,YouTube,Default}
