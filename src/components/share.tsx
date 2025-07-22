00839bfaf4fcca0e664079da354aa444a011bc05
Reijan <reijan.lj@gmail.com>
2025-07-22 14:56:02 -0300

    fix/spaces

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -14,8 +14,8 @@ export default function Share({title}: {title: string}) {
         await navigator.share({
           title: title,
           text: `Click no link para mais detalhes 👇
-
-`,
+          
+          `,
           url: `${site_url}${pathname}`,
         });
       } catch (error) {
