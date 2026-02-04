f9d4474244ff9e8a2875a5ffed49d525cb3743d4
Marcos Ramos <marcosperes.contato@gmail.com>
2026-02-04 20:57:18 -0300

    hotfix-revalidation fails

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -11,6 +11,7 @@ type WebhookPayload = {
 export async function POST(req: NextRequest) {
   try {
      const secret = req.headers.get('secret')
+     const body = await req.json() as WebhookPayload
 
     if (!process.env.REVALIDATE_SECRET_TOKEN) {
       return new Response('Missing environment variable REVALIDATE_SECRET_TOKEN', {status: 500})
@@ -20,20 +21,21 @@ export async function POST(req: NextRequest) {
     return NextResponse.json({ message: 'Invalid token' }, { status: 401 })
   }
 
-    const {isValidSignature, body} = await parseBody<WebhookPayload>(
-      req,
-      process.env.REVALIDATE_SECRET_TOKEN,
-    )
+    // const {isValidSignature, body} = await parseBody<WebhookPayload>(
+    //   req,
+    //   process.env.REVALIDATE_SECRET_TOKEN,
+    // )
 
-    if (!isValidSignature) {
-      const message = 'Invalid signature'
-      return new Response(JSON.stringify({message, isValidSignature, body}), {status: 401})
-    } else if (!body?._type) {
-      const message = 'Bad Request'
-      return new Response(JSON.stringify({message, body}), {status: 400})
-    }
+    // if (!isValidSignature) {
+    //   const message = 'Invalid signature'
+    //   return new Response(JSON.stringify({message, isValidSignature, body}), {status: 401})
+    // } else if (!body?._type) {
+    //   const message = 'Bad Request'
+    //   return new Response(JSON.stringify({message, body}), {status: 400})
+    // }
 
-    revalidateTag(body._type)
+    // revalidateTag(body._type)
+    console.log(`Revalidating path for type: ${body._type}`)
 
     return NextResponse.json({body})
   } catch ( err: any) {
